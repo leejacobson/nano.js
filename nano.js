8fdd12b6a60cab74a2aeda2932049a677cb134b2
Lee Jacobson <lee.jacobson@clarks.com>
2020-09-21 18:14:09 +0100

    Removed log() and animation() Code cleanup

--- nano.js
+++ nano.js
@@ -51,9 +51,7 @@ class Nano {
 
     attr(a, v = null) {
         if (!v && v != '') return this.getAttr(a);
-        return this.each(function(i) {
-            i.el[0].setAttribute(a, v);
-        });
+        return this.each(i => i.el[0].setAttribute(a, v));
     }
 
     get(i) {
@@ -63,9 +61,7 @@ class Nano {
 
     children(s) {
         let els = [];
-        this.each(function(i) {
-            els.push(Array.from(i.el[0].children).reduce(curEl => curEl.matches(s)));
-        });
+        this.each(i => els.push(Array.from(i.el[0].children).reduce(curEl => curEl.matches(s))));
         return new Nano(els);
     }
 
@@ -87,19 +83,16 @@ class Nano {
     }
 
     css(v) {
-        return this.each(function(i) {
-            i.el[0].style.cssText = i.el[0].style.cssText + v;
-        });
+        return this.each(i => i.el[0].style.cssText = i.el[0].style.cssText + v);
     }
 
     style(v) {
-        this.each(function(i) {
+        return this.each(i => {
             for (var key in v) {
                 if (!v[key]) return i.el[0].style.removeProperty(key);
                 i.el[0].style[key] = v[key];
             }
         });
-        return this;
     }
 
     data(a, v) {
@@ -122,33 +115,22 @@ class Nano {
     }
 
     removeAttr(v) {
-        return this.each(function(i) {
-            i.el[0].removeAttribute(v);
-        });
-    }
-
-    animate(time, scale, rotate, rotateX, rotateY, translateX, translateY, skewX, skewY, opacity) {
-        return this.each(function(i) {
-            i.el[0].style.cssText = `${i.style.cssText}transition: all ${time}s ease-in-out;
-                transform: scale(${scale}) rotate(${rotate}deg) rotateX(${rotateX}deg) rotateY(${rotateY}'deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg);
-                opacity: ${opacity};`;
-        });
+        return this.each(i => i.el[0].removeAttribute(v));
     }
 
     on(type, fn) {
         if (typeof type == 'string') type = [type];
-        this.each(function(i) {
+        return this.each(i => {
             type.forEach(thisType => {
                 let event = nanoEvent.add(thisType, fn);
                 i.el[0].addEventListener(event.eventType, event.handler, true);
             });
         });
-        return this;
     }
 
     unbind(type, fn = null) {
         if (typeof type == 'string') type = [type];
-        return this.each(function(i) {
+        return this.each(i => {
             type.forEach(thisType => {
                 let matchedEvents = nanoEvent.find(thisType, fn);
                 matchedEvents.forEach(event => {
@@ -165,7 +147,7 @@ class Nano {
 
     addClass(v) {
         var a = v.split(' ');
-        return this.each(function(i) {
+        return this.each(i => {
             for (var x = 0; x < a.length; x++) {
                 if (i.el[0].classList) {
                     i.el[0].classList.add(a[x]);
@@ -178,7 +160,7 @@ class Nano {
 
     toggleClass(v) {
         var a = v.split(' ');
-        return this.each(function(i) {
+        return this.each(i => {
             for (var x = 0; x < a.length; x++) {
                 i.el[0].classList.toggle(a[x]);
             }
@@ -191,7 +173,7 @@ class Nano {
 
     removeClass(v) {
         var a = v.split(' ');
-        return this.each(function(i) {
+        return this.each(i => {
             for (var x = 0; x < a.length; x++) {
                 i.el[0].classList.remove(a[x]);
             }
@@ -203,7 +185,7 @@ class Nano {
     }
 
     hide() {
-        this.each(function(i) {
+        return this.each(i => {
             i.attr('hidden', '');
             i.style({ display: 'none' });
         });
@@ -211,13 +193,11 @@ class Nano {
 
     html(v) {
         if (!v && v !== '') return this.el[0].innerHTML;
-        return this.each(function(i) {
-            i.el[0].innerHTML = v;
-        });
+        return this.each(i => i.el[0].innerHTML = v);
     }
 
     show() {
-        return this.each(function(i) {
+        return this.each(i => {
             i.removeAttr('hidden');
             i.style({ display: this.getDefaultValue('display') });
         });
@@ -226,7 +206,7 @@ class Nano {
     text(v) {
         if (this.length == 0) return null;
         if (typeof v == 'undefined') return this.el[0].innerText || this.el[0].textContent;
-        return this.each(function(i) {
+        return this.each(i => {
             i.el[0].innerText = v;
             i.el[0].textContent = v;
         });
@@ -235,13 +215,9 @@ class Nano {
     _insertAdjacent(v, position) {
         if (typeof v === 'object') {
             if (v instanceof Nano) v = v[0];
-            return this.each(function(i) {
-                i.el[0].insertAdjacentElement(position, v);
-            });
+            return this.each(i => i.el[0].insertAdjacentElement(position, v));
         } else {
-            return this.each(function(i) {
-                i.el[0].insertAdjacentHTML(position, v);
-            });
+            return this.each(i => i.el[0].insertAdjacentHTML(position, v));
         }
     }
 
@@ -262,9 +238,7 @@ class Nano {
     }
 
     empty() {
-        return this.each(function(i) {
-            i.el[0].innerHTML = '';
-        });
+        return this.each(i => i.el[0].innerHTML = '');
     }
 
     parent() {
@@ -276,17 +250,12 @@ class Nano {
         return this.el[0].tagName.toLowerCase() == t.toLowerCase();
     }
 
-    log() {
-        console.log(this);
-    }
-
     value(val) {
         if (typeof val === 'undefined') return this.el[0].value;
-        this.each(function(i) {
+        return this.each(i => {
             i.el[0].value = val;
             i.el[0].dispatchEvent(new CustomEvent('input'));
         });
-        return this;
     }
 
     focus() {
@@ -306,13 +275,7 @@ class Nano {
             func();
             return this;
         }
-        document.addEventListener(
-            'DOMContentLoaded',
-            () => {
-                func();
-            },
-            false
-        );
+        document.addEventListener('DOMContentLoaded', () => func(), false);
         return this;
     }
 
